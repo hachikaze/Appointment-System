3750b22b7a691e4c29a8d819c525e5793f6be3c2
Michael <michaeljohn.labton1@gmail.com>
2025-02-20 09:32:18 +0800

    Resolved Merge

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,6 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
-=======
 use App\Notifications\CustomVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -17,7 +9,6 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 class User extends Authenticatable implements MustVerifyEmail
->>>>>>> 22ae4e9a2a3c5770754365515ea942f132985fb9
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -27,17 +18,9 @@ class User extends Authenticatable implements MustVerifyEmail
      *
      * @var list<string>
      */
-<<<<<<< HEAD
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-=======
     protected $table = 'users';
 
     protected $guarded = [];
->>>>>>> 22ae4e9a2a3c5770754365515ea942f132985fb9
 
     /**
      * The attributes that should be hidden for serialization.
@@ -61,12 +44,9 @@ class User extends Authenticatable implements MustVerifyEmail
             'password' => 'hashed',
         ];
     }
-<<<<<<< HEAD
-=======
 
     public function sendEmailVerificationNotification()
     {
         $this->notify(new CustomVerifyEmail);
     }
->>>>>>> 22ae4e9a2a3c5770754365515ea942f132985fb9
 }
